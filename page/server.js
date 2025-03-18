94f84faa0f2e1597a59a43f8f0da40bb6fdeacfa
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2025-03-18 18:57:09 +0000

    NE LEVEL

--- page/server.js
+++ page/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
@@ -12,7 +16,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// GET request for harvest.html
+app.get('/harvest', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/harvest.html'));
+});
+
+// POST request to redirect to harvest.html
+app.post('/harvest', (req, res) => {
+    res.redirect('/harvest');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
